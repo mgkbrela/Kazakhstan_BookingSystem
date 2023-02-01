cab58f8cf467b94741aa9299fadbf788f5c922ab
mgkbrela <98755684+mgkbrela@users.noreply.github.com>
2023-02-02 01:19:13 +0900

    pics and navbar

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
     <style>
     	header.masthead {
-		  background: url(assets/img/<?php echo $_SESSION['setting_cover_img'] ?>);
+		  background: url(assets/img/cover.jpg);
 		  background-repeat: no-repeat;
 		  background-size: cover;
 		}
@@ -27,7 +27,7 @@
       </div>
         <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="./"><?php echo $_SESSION['setting_hotel_name'] ?></a>
+                <a class="navbar-brand" href = "index.php"> <img src = "assets/img/logo.png"> </a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto my-2 my-lg-0">
